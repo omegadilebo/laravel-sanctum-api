61a09ec976e1964f3f7bae0a55bd675a5436c434
omegadilebo <omegameseret@yahoo.com>
2021-05-31 15:02:10 +0300

    added assets

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Application;
+use Illuminate\Http\Request;
+
+class ApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Application::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'applicant_name' => 'required',
+            'birthday' => 'required',
+            'grade_level' => 'required',
+            'guardian_name' => 'required',
+            'guardian_type' => 'required',
+            'country' => 'required',
+            'address1' => 'required',
+            'postal_code' => 'required'
+        ]);
+
+        return Application::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Application::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $application = Application::find($id);
+        $application->update($request->all());
+        return $application;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Application::destroy($id);
+    }
+
+     /**
+     * Search for a name
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Application::where('applicant_name', 'like', '%'.$name.'%')->get();
+    }
+}
